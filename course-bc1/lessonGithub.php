9a8a2dd88ff7bf59b81a08eade69ac60df0ae28d
Matthew Higley <matthewhigley@MattyBook-Pro.local>
2016-07-26 17:09:20 -0700

    lessonGithub update

--- course-bc1/lessonGithub.php
+++ course-bc1/lessonGithub.php
@@ -263,8 +263,10 @@ git push</pre></code>
             GitHub is a Git repository hosting service, but it adds many of its own features. While Git is a command line tool, GitHub provides a Web-based graphical interface. It also provides access control and several collaboration features, such as a wikis and basic task management tools for every project.
         </p>
         <div class="figure">
-            <img src="/img/github.png" alt="Github | Social Coding" />
-            <figcaption>Source: <a href="http://itcloudworks.com/google-cloud-source-repositories-going-after-github/" target="_blank">itcloudworks.com</a></figcaption>
+            <figure>
+                <img src="/img/github.png" alt="Github | Social Coding" />
+                <figcaption>Source: <a href="http://itcloudworks.com/google-cloud-source-repositories-going-after-github/" target="_blank">itcloudworks.com</a></figcaption>
+            </figure>
         </div>
     </article>
 </section>
